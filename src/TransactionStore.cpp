a4fee89e66f725659037c37c71dc3d4a41d110e9
Mateusz Zarudzki <zarudzki@9livesdata.com>
2019-02-26 15:17:58 +0100

    store whole transactions in hash table of trees

--- src/TransactionStore.cpp
+++ src/TransactionStore.cpp
@@ -2,20 +2,77 @@
 #include "Database.h"
 
 
+
+
+
+//virtual std::vector<Transaction> findTransactions(const std::string &accNo) = 0;
+
+
+//virtual double calculateAverageAmount(const std::string &accNo) = 0;
+
+/**
+ * Sets list of all transactions (data source) to be used by other methods.
+ */
+//virtual void setTransactions(const std::vector<Transaction> &transactions) = 0;
+
+/**
+ * Finds one transaction by its account number and transaction id.
+ * If there are duplicates (by accNo, txNo) returns first matching.
+ *
+ * Throws exception if not found.
+ */
 Transaction TransactionStore::findTransaction(const std::string &accNo, int txNo) {
-    return transactions[0];
+
+    if (transactions_hashed.count(accNo) == 0)
+        throw std::exception();
+
+    if (transactions_hashed[accNo].count(txNo) == 0)
+        throw std::exception();
+
+    return transactions_hashed[accNo][txNo];
 }
 
+/**
+ * Finds all transactions with given account number.
+ * Transactions are sorted by txNo without duplicates (same accNo, txNo).
+ */
 std::vector<Transaction> TransactionStore::findTransactions(const std::string &accNo) {
-    return {transactions[0]};
+
+    std::vector<Transaction> transactions;
+
+    for (const auto& trans_map : transactions_hashed[accNo]){
+        transactions.push_back(trans_map.second);
+    }
+
+    return transactions;
 }
+
+/**
+ * Calculates average amount of all transactions with given account number.
+ */
 
 double TransactionStore::calculateAverageAmount(const std::string &accNo) {
-    return 0.0;
+
+    int no_transactions = transactions_hashed.count(accNo);
+
+    if (!no_transactions)
+        return 0.0;
+
+    double total_amount = 0.0;
+
+    for (const auto& trans : transactions_hashed[accNo]) {
+        total_amount += trans.second.amount;
+    }
+
+    return static_cast<double>(total_amount / no_transactions);
 }
 
 void TransactionStore::setTransactions(const std::vector<Transaction> &transactions) {
-    this->transactions = transactions;
+
+    for (const auto& trans : transactions) {
+        transactions_hashed[trans.accNo][trans.txNo] = trans;
+    }
+
 }
 
 
--- src/TransactionStore.h
+++ src/TransactionStore.h
@@ -3,6 +3,9 @@
 #define INTERVIEW_TRANSACTIONSTORE_H
 
 #include "Database.h"
+#include <unordered_map>
+#include <map>
+
 
 class TransactionStore : public Database {
 public:
@@ -11,7 +14,8 @@ public:
     double calculateAverageAmount(const std::string &accNo) override;
     void setTransactions(const std::vector<Transaction> &transactions) override;
 private:
-    std::vector<Transaction> transactions;
+//    std::vector<Transaction> transactions;
+    std::unordered_map<std::string, std::map<unsigned int, Transaction> > transactions_hashed;
 };
 
 #endif //INTERVIEW_TRANSACTIONSTORE_H
